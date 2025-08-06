41ad901df5a488136a3f4282948ad38bb90a9c19
Avinash <129788500+babloo004@users.noreply.github.com>
2025-08-07 01:39:55 +0530

    Add files via upload

---
+++ src/wish.groovy
@@ -0,0 +1,11 @@
+def gm(def name) {
+    echo "Good Morning ${name}!"
+}
+
+def ga(def name) {
+    echo "Good Afternoon ${name}!"
+}
+
+def ge(def name) {
+    echo "Good Evening ${name}!"
+}
---
+++ vars/wish.groovy
@@ -0,0 +1,15 @@
+
+//good morning
+def gm(String name){
+  echo "Good Morning ${name}!"
+}
+
+//good afternoon
+def ga(String name){
+  echo "Good Afternoon ${name}!"
+}
+
+//good evening
+def ge(String name){
+  echo "Good Evening ${name}!"
+}
